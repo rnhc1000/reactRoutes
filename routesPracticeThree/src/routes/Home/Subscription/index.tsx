837910409a87fa039500ba2218708b0492622d91
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-09 22:33:26 -0300

    Ex 3

---
+++ routesPracticeThree/src/routes/Home/Subscription/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
+export default function Subscription() {
+
+    return (
+        <main>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card title="Página de Inscrição" />
+                    </div>
+                    <div className="dflex">
+                        <Link to="/promotion">
+                            <Button text="Ver Promoção" />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
